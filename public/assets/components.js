89ba1913bb8a83143fefcf3f8045a89b69ab3177
Michał Ordon <michal@tehidea.com>
2016-04-30 00:18:13 +0100

    Measure performance with transpiring within browser

--- public/assets/components.js
+++ public/assets/components.js
@@ -266,6 +266,7 @@ class CommentRemoveConfirmation extends React.Component {
 jQuery(function() {
   ReactDOM.render(
     <CommentBox />,
-    document.getElementById('comment-box')
+    document.getElementById('comment-box'),
+    console.timeEnd('react-app')
   );
 })
