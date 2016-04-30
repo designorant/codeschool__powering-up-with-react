7761db9fb8226b284507828b69d7920a6af3ad6f
Michał Ordon <michal@tehidea.com>
2016-04-30 01:06:10 +0100

    Extract confirmation component

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CommentRemoveConfirmation from './comment-remove-confirmation';
+import CommentConfirmation from './comment-confirmation';
 
 export default class Comment extends React.Component {
   constructor() {
@@ -29,16 +29,18 @@ export default class Comment extends React.Component {
         <p className="comment-body">{commentBody}</p>
 
         <div className="comment-actions">
-          <CommentRemoveConfirmation onDelete={this._handleDelete.bind(this)} />
-          <a href="#" onClick={this._toggleAbuse.bind(this)}>Report as Abuse</a>
+          <CommentConfirmation onConfirm={this._handleDelete.bind(this)}>
+            Delete Comment?
+          </CommentConfirmation>
+          <CommentConfirmation onConfirm={this._toggleAbuse.bind(this)}>
+            Report as Abuse
+          </CommentConfirmation>
         </div>
       </div>
     );
   }
 
-  _toggleAbuse(event) {
-    event.preventDefault();
-
+  _toggleAbuse() {
     this.setState({
       isAbusive: !this.state.isAbusive
     });
