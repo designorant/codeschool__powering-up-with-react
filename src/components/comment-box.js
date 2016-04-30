1f6aa5bb310ea65f68d59dbcb8144bdc42454c1a
Michał Ordon <michal@tehidea.com>
2016-04-30 02:06:23 +0100

    Refactor bindings

--- src/components/comment-box.js
+++ src/components/comment-box.js
@@ -14,6 +14,10 @@ export default class CommentBox extends React.Component {
       showComments: false,
       comments: []
     };
+
+    this._addComment = this._addComment.bind(this);
+    this._deleteComment = this._deleteComment.bind(this);
+
   }
 
   componentWillMount() {
@@ -27,7 +31,7 @@ export default class CommentBox extends React.Component {
         <div className="cell">
           <h2>Join The Discussion</h2>
           <div className="comment-box">
-            <CommentForm addComment={this._addComment.bind(this)} />
+            <CommentForm addComment={this._addComment} />
             <CommentAvatarList avatars={this._getAvatars()} />
 
             {this._getPopularMessage(comments.length)}
@@ -59,7 +63,7 @@ export default class CommentBox extends React.Component {
     return this.state.comments.map((comment) => {
       return <Comment
                {...comment}
-               onDelete={this._deleteComment.bind(this)}
+               onDelete={this._deleteComment}
                key={comment.id} />
     });
   }
